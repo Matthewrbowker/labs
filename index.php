4edfe75c7f1f59b67605f6ae96dabdf753a16a12
Matthew Bowker <matthewrbowker@me.com>
2014-01-10 15:34:27 -0700

    Update cross-namespace redirect tool to latest version

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 require("includes.php");
 
-$site = new site();
+//$site = new site();
 
-$site ->gen_opening();
+//$site ->gen_opening();
 
 if (isset($_GET['test'])) $testserver = true;
 
@@ -11,9 +11,13 @@ $bots = "<b>Bots</b>\n<ul>\n";
 $tools = "<b>Tools</b>\n<ul>\n";
 $test = "<b>Testing tools</b>\n<ul>\n";
 
-$data = parse_ini_file('vars/versions.ini',true);
+//$data = parse_ini_file('vars/versions.ini',true);
 
-$keys = array_keys($data);
+//$keys = array_keys($data);
+
+echo memory_get_usage();
+
+die();
 
 
 for($i=0,$c=count($data);$i<$c;$i++) {
