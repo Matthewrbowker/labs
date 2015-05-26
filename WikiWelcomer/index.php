2ab7e0cf4aaacd6613036c1b9be998cb0c5fdc6b
Matthew Bowker <matthewrbowker@me.com>
2015-05-25 18:34:57 -0600

    Minor fix for WikiWelcomer

--- WikiWelcomer/index.php
+++ WikiWelcomer/index.php
@@ -10,7 +10,7 @@ $site -> gen_opening();
 //new_genheader();?>
 
 WikiWelcomer welcomes people when they join the help channel (<a href="ircs://freenode/wikipedia-en-help">#wikipedia-en-help on Freenode</a>).  It is a backup system in case <a href="http://en.wikipedia.org/wiki/WP:HELPMEBOT">Helpmebot</a> is down.  To activate the bot, change <a href="http://en.wikipedia.org/wiki/User:WikiWelcomer/control" target=_blank>this page</a> to "on".
-<?
+<?php
 //endtime($start);
 //new_genfooter();
 $site -> gen_closing();
