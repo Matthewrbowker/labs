3df347787ff4ee472f96ce498f9a522672f3444e
Matthew Bowker <matthewrbowker@me.com>
2016-01-02 02:14:07 -0700

    Fix the conditional check

--- includes.php
+++ includes.php
@@ -6,7 +6,7 @@ require('vars/functions.php'); // Functions
 
 // Configuration variables
 require('vars/vars.php'); //Starting config variables
-if (file_exists("vars/vars. . $_SERVER[SERVER_NAME] . .php")) {require("vars/vars." . $_SERVER["SERVER_NAME"] . ".php"); }
+if (file_exists("vars/vars." . $_SERVER["SERVER_NAME"] . ".php")) {require("vars/vars." . $_SERVER["SERVER_NAME"] . ".php"); }
 
 // More config files can go here.
 
